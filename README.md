855483e4b4cae0e657578e64e15a232229643d86
Arek Sredzki <arek@sredzki.com>
2015-10-05 23:16:55 -0700

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# arduino-scanner
-An NPM module which makes it easy to detect an arduino connected to any device!
+# ArduinoScanner
+An NPM module which makes it easy to detect an Arduino connected to any device!
+
+## Usage
+Get it through NPM by running: `npm install arduino-scanner`
+
+### Troubleshooting
+Node 0.12.7 is required until the `serialport` module is updated.
+
+**Node 4.0 is NOT supported at this time.**
+
+### Usage Example
+
+```node
+var ArduinoScanner = require('arduino-scanner');
+var arduinoScanner = new ArduinoScanner();
+
+arduinoScanner.on('arduinoFound', function(response) {
+  arduinoScanner.stop();
+  console.log(response.message);
+  connectToArduino(response.port);
+});
+```
+
+## Reference Guide
+### Methods
+#### .start (interval)
+Starts scanning for valid Arduino serial ports.
+It will emit an `arduinoFound` event once a matching port is found.
+
+##### interval
+The time in milliseconds before trying port reads again.
+The default is `500ms`.
+
+#### .stop ()
+Stop scanning for Arduinos.
+
+### Events
+#### .on('arduinoFound', callback)
+Passes an object to the callback function containing the `port` comName and a `message`.
+
+#### .on('arduinoNotFound', callback)
+Passes an object to the callback function containing the `port` comName and a `message`.
+
+#### .on('noPortsFound', callback)
+This is potentially useful as a debugging tool. If no ports are found this will be called, it will pass an object containing a `message` to the callback.
+
+## Roadmap
+- Add support for more Arduino boards.
+
+## Currently supported boards
+- Arduino/Genuino Mega 2560
+- Sparkfun Pro Mega
+
+_Please create pull requests adding additional boards, all you need is the vendor & product IDs_
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,88 @@
+/**
+ * A module which scans for available arduino ports, and
+ * emits an event when an valid port is found.
+ *
+ * @author UBCSailbot
+ * @author areksredzki
+ * @author joshuabaker2
+ */
+
+var util = require("util");
+var EventEmitter = require("events").EventEmitter;
+var serialPort = require('serialport');
+
+function ArduinoScanner() {
+  var self = this;
+
+  var searchInterval;
+
+  EventEmitter.call(self);
+
+  /**
+   * Searches the serial ports for any device that has a vendor id and product id
+   * that matches the arduino's. It only emits the first Arduino it finds. If you
+   * want it to return multiple Arduinos, take out the 'return matched' within the
+   * ports.some(function(port){}). If you want more information on all the ports
+   * that it is skipping over, pass 'true' as the second parameter to scan.start()
+   *
+   * i.e.
+   * scan.start(500, true)
+   */
+  var search = function() {
+    serialPort.list(function(err, ports) {
+      if (ports.length === 0) {
+        self.emit('noPortsFound', {
+          message: 'Nothing detected in serial ports. Check connections.'
+        });
+        return;
+      }
+      ports.some(function(port) {
+        var matched =
+          (port.vendorId == '0x0403' && port.productId === '0x6001') || // Pro Mega
+          (port.vendorId == '0x2341' && port.productId === '0x0042'); // Arduino.cc
+
+        if (matched) {
+          self.emit('arduinoFound', {
+            port: port.comName,
+            message: 'Arduino found at port ' + port.comName + '.'
+          });
+        } else {
+          if (_verbose) {
+            self.emit('arduinoNotFound', {
+              message: 'Arduino not at port ' + port.comName + '.'
+            });
+          }
+        }
+        return matched;
+      });
+    });
+  };
+}
+
+/**
+ * Starts scanning for valid Arduino serial ports.
+ * It will emit an 'arduinoFound event once a port is found.
+ * @param interval - time in milliseconds before trying port reads again.
+ */
+ArduinoScanner.prototype.start = function(interval) {
+  // If the interval isn't set, default to 500 ms.
+  interval = typeof interval !== 'undefined' ? interval : 500;
+
+  searchInterval = setInterval(search, interval);
+};
+
+/**
+ * Stop searching.
+ */
+ArduinoScanner.prototype.stop = function() {
+  if (searchInterval) {
+    clearInterval(searchInterval);
+    console.log('Arduino scan stopped.');
+  } else {
+    console.log('Arduino scan was not active.');
+  }
+};
+
+util.inherits(ArduinoScanner, EventEmitter);
+
+module.exports = ArduinoScanner;
