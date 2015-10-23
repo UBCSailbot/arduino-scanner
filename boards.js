243fdf885c5616d71bd62d74916c62fedaf95504
Arek Sredzki <arek@sredzki.com>
2015-10-23 00:30:11 -0700

    Consolidated sf-mega-pro with mega for compatibility with avrgirl

--- boards.js
+++ boards.js
@@ -5,7 +5,7 @@ module.exports = {
   '0x0042': ['mega'],
   '0x2404': ['blend-micro'],
   '0x516d': ['qduino'],
-  '0x6001': ['sf-mega-pro', 'nano', 'duemilanove168'],
+  '0x6001': ['mega', 'nano', 'duemilanove168'],
   '0x6015': ['tinyduino'],
   '0x6051': ['pinoccio'],
   '0x8036': ['imuduino', 'leonardo', 'little-bits'],
